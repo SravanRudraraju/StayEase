5892deaaa76077c4e59b35c074a0b8d858c86833
SravanRudraraju <sravanrudraraju@gmail.com>
2026-01-07 16:07:26 +0530

    show listing page styling addded

--- models/listing.js
+++ models/listing.js
@@ -9,11 +9,8 @@ const listingSchema = new Schema({
     description: String,
     image: {
         type: String,
-        url: {
-            type: String,
-            default: "https://plus.unsplash.com/premium_photo-1669748157617-a3a83cc8ea23?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1669748157617-a3a83cc8ea23?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-        }
+        default: "https://plus.unsplash.com/premium_photo-1669748157617-a3a83cc8ea23?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          set: (v) =>v && v.trim() !== ""? v : "https://plus.unsplash.com/premium_photo-1669748157617-a3a83cc8ea23?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     price: Number,
     location: String,
